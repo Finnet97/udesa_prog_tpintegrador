e48238cd727dca0dfc3f948e50d1999798f49d02
Finnet97 <totochipont@gmail.com>
2024-11-20 12:54:10 -0300

    CSS basico de pagina de registro + boton para anadir mas comidas en index

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
 
-fetch('https://dummyjson.com/recipes')
 
+let currentPage = 1;
+const recipesPerPage = 12;
+
+const recetaList = document.querySelector(".recetas-container");
+const btnCargarMas = document.getElementById("btn-cargar-mas");
+
+fetch(`https://dummyjson.com/recipes?skip=0&limit=${recipesPerPage}`)
 .then(function(response) {
     return response.json();
 })
 .then(function(data) {
-    console.log(data);
-
-    const recetaList = document.querySelector(".recetas-container");
-
     let recetas = "";
 
-    for (let i = 0; i < 12; i++) {
+    for (let i = 0; i < data.recipes.length; i++) {
         const receta = data.recipes[i];
 
         recetas += `
@@ -26,7 +28,46 @@ fetch('https://dummyjson.com/recipes')
     }
 
     recetaList.innerHTML = recetas;
+
+    if (data.recipes.length < recipesPerPage) {
+        btnCargarMas.style.display = "none";
+    }
 })
 .catch(function(error) {
     console.log("Error: " + error);
+});
+
+btnCargarMas.addEventListener("click", function() {
+    currentPage++;
+
+    fetch(`https://dummyjson.com/recipes?skip=${(currentPage - 1) * recipesPerPage}&limit=${recipesPerPage}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(data) {
+            let recetas = "";
+
+            for (let i = 0; i < data.recipes.length; i++) {
+                const receta = data.recipes[i];
+
+                recetas += `
+                    <a href="./receta.html?id=${receta.id}">
+                        <article>
+                            <img src="${receta.image}" alt="${receta.name}">
+                            <p>${receta.name}</p>
+                            <p>${receta.difficulty}</p>
+                        </article>
+                    </a>
+                `;
+            }
+
+            recetaList.innerHTML += recetas;
+
+            if (data.recipes.length < recipesPerPage) {
+                btnCargarMas.style.display = "none";
+            }
+        })
+        .catch(function(error) {
+            console.log("Error: " + error);
+        });
 });
