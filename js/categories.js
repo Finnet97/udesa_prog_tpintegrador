14f802c14b70177798c3148c653e158d5261de2f
Finnet97 <totochipont@gmail.com>
2024-11-22 12:05:32 -0300

    Pequenos cambios en algunas cosas, como en la pagina de detalles y el boton de cargar mas categorias

--- js/categories.js
+++ js/categories.js
@@ -1,9 +1,8 @@
 
-let currentPage = 1;
-const recipesPerPage = 12;
+const categoriesPerPress = 12;
 
 const categoriaContainer = document.querySelector(".categories");
-const btnCargarMasCats = document.getElementById("btn-cargar-mas");
+const btnCargarMasCats = document.getElementById("btn-cargar-mas-categories");
 
 fetch(`https://dummyjson.com/recipes/tags`)
 .then(function(response) {
@@ -25,44 +24,33 @@ fetch(`https://dummyjson.com/recipes/tags`)
     }
 
     categoriaContainer.innerHTML += cat;
-
-    if (data.length >= tags.length) {
-        loadMoreBtn.style.display = "none";
-    }
 })
 .catch(function(error) {
     console.log("Error: " + error);
 });
 
 btnCargarMasCats.addEventListener("click", function() {
-    currentPage++;
 
     fetch(`https://dummyjson.com/recipes/tags`)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        let recetas = "";
+        let nuevasCats = "";
 
-        for (let i = 0; i < data.recipes.length; i++) {
-            const receta = data.recipes[i];
+        for (let i = 0; i < categoriesPerPress; i++) {
+            const categoria = data[i];
 
-            recetas += `
-                <a href="./receta.html?id=${receta.id}">
+            nuevasCats += `
+                <a href="./index.html">
                     <article>
-                        <img src="${receta.image}" alt="${receta.name}">
-                        <p>${receta.name}</p>
-                        <p>${receta.difficulty}</p>
+                        <p>${categoria}</p>
                     </article>
                 </a>
             `;
         }
 
-        recetaList.innerHTML += recetas;
-
-        if (data.recipes.length < recipesPerPage) {
-            btnCargarMas.style.display = "none";
-        }
+        categoriaContainer.innerHTML += nuevasCats;
     })
     .catch(function(error) {
         console.log("Error: " + error);
