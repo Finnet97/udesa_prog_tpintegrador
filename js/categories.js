0052603e0ddd31d32999382fe4bdbd17226e3228
Finnet97 <totochipont@gmail.com>
2024-11-22 11:40:45 -0300

    Se aniadio un boton para cargar mas categorias en categories, falta funcionalidad completa

--- js/categories.js
+++ js/categories.js
@@ -1,5 +1,9 @@
 
+let currentPage = 1;
+const recipesPerPage = 12;
+
 const categoriaContainer = document.querySelector(".categories");
+const btnCargarMasCats = document.getElementById("btn-cargar-mas");
 
 fetch(`https://dummyjson.com/recipes/tags`)
 .then(function(response) {
@@ -21,7 +25,46 @@ fetch(`https://dummyjson.com/recipes/tags`)
     }
 
     categoriaContainer.innerHTML += cat;
+
+    if (data.length >= tags.length) {
+        loadMoreBtn.style.display = "none";
+    }
 })
 .catch(function(error) {
     console.log("Error: " + error);
+});
+
+btnCargarMasCats.addEventListener("click", function() {
+    currentPage++;
+
+    fetch(`https://dummyjson.com/recipes/tags`)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        let recetas = "";
+
+        for (let i = 0; i < data.recipes.length; i++) {
+            const receta = data.recipes[i];
+
+            recetas += `
+                <a href="./receta.html?id=${receta.id}">
+                    <article>
+                        <img src="${receta.image}" alt="${receta.name}">
+                        <p>${receta.name}</p>
+                        <p>${receta.difficulty}</p>
+                    </article>
+                </a>
+            `;
+        }
+
+        recetaList.innerHTML += recetas;
+
+        if (data.recipes.length < recipesPerPage) {
+            btnCargarMas.style.display = "none";
+        }
+    })
+    .catch(function(error) {
+        console.log("Error: " + error);
+    });
 });
