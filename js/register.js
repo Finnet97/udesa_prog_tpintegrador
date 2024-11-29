cfc7d6f79c7bdf0b543885c74464dd9e87e5e5b6
Juan Ignacio <ignaciopassarini@gmail.com>
2024-11-29 20:23:37 -0300

    Arreglo de funcionamiento de register.js

--- js/register.js
+++ js/register.js
@@ -1,38 +1,37 @@
 const loginForm = document.querySelector(".formLogin");
-const buscadorIndex = document.querySelector("form-busqueda");
+const buscadorIndex = document.querySelector("#form-busqueda");
 
 loginForm.addEventListener('submit', function(event) {
-    const email = document.querySelector('email').value;
-    const password = document.querySelector('password').value;
-    const terminos = document.querySelector('terminos').checked;
+    
+    const email = document.querySelector('#email').value;
+    const password = document.querySelector('#password').value;
+    const terminos = document.querySelector('#terminos').checked;
+    event.preventDefault();
 
     if (email == "") {
         errores("E-Mail Invalido");
-        event.preventDefault();
-    }
-
-    if (password == "") {
+    } else {
+        if (password == "") {
         errores("Contraseña Invalida");
-        event.preventDefault();
-    }
-
-    if (!terminos) {
-        errores("Aceptar TyC");
-        event.preventDefault();
+        } else {
+            if (!terminos) {
+                errores("Aceptar TyC");
+            }
+        }
     }
 });
 
 let errorMensaje = "";
 
 function errores(msj) {
-    errorMensaje += msj
-    document.querySelector('errorMensaje').style.display = "block";
-    document.querySelector('errorMensaje').innerHTML = errorMensaje;
+    errorMensaje = msj
+    document.querySelector('#errorMensaje').style.display = "block";
+    document.querySelector('#errorMensaje').innerHTML = errorMensaje;
 }
 
 buscadorIndex.addEventListener('submit', function(event) {
 
-    const inputBusqueda = document.querySelector('input-busqueda').value;
+    const inputBusqueda = document.querySelector('#input-busqueda').value;
 
     if (inputBusqueda == "") {
         alert("El buscador esta vacío, probá poniendo texto.");
